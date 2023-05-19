0acb57e888fda535129cedc9f4dea8dbef8c64fb
fa7ehak <fa7ehak@gmail.com>
2023-05-19 19:53:22 +0530

    chore: Setup Husky and lint-staged for linted commits

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+const prettierCmd = `prettier --loglevel warn --plugin-search-dir=. --cache --cache-strategy content --cache-location ./node_modules/.cache/.prettiercache --write`;
+const stylelintCmd = `stylelint --max-warnings=0 --custom-formatter=node_modules/stylelint-formatter-pretty --aei --cache --cache-strategy content --cache-location ./node_modules/.cache/.stylelintcache --fix`;
+const eslintCmd = `eslint --max-warnings=0 --format=pretty --cache --cache-strategy content --cache-location ./node_modules/.cache/.eslintcache`;
+
+module.exports = {
+  '**/*.{js,ts,jsx,tsx}': [eslintCmd, prettierCmd],
+  '**/*.css': [stylelintCmd, prettierCmd],
+  '**/*.json': [prettierCmd],
+};
